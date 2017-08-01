ec83d3bc3080ae7cf46964f6ddff0b10af78f98e
SkylineBin <bin@skylinebin.com>
2017-08-01 14:06:22 +0800

    add two little demo from windows

---
+++ NumpyandPandas/numpystudy_ex_1.py
@@ -0,0 +1,12 @@
+'''# _*_ coding : UTF-8 _*_'''
+#coding=utf-8
+
+import numpy as np
+import pandas as pd
+
+array = np.array([[1,2,3],[2,3,4]])
+
+print(array)
+print('number of dim:',array.ndim) #输出维数
+print('shape:',array.shape) #输出n*m的矩阵表示(n,m)
+print('size:',array.size) #输出矩阵里的元素的数目
---
+++ NumpyandPandas/numpystudy_ex_2.py
@@ -0,0 +1,7 @@
+'''# _*_ coding : UTF-8 _*_'''
+#coding=utf-8
+
+import numpy as np
+import pandas as pd
+
+a = np.array()
