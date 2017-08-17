98640eaf036f28c32d3b1d8e04035fec3f3a95df
SkylineBin <bin@skylinebin.com>
2017-08-17 14:42:29 +0800

    add Sarsalamda Example

--- Sarsalamba/RL_brain.py
+++ Sarsalamba/RL_brain.py
@@ -1,9 +1,3 @@
-"""
-This part of code is the Q learning brain, which is a brain of the agent.
-All decisions are made in here.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
-
 import numpy as np
 import pandas as pd
 
@@ -45,32 +39,48 @@ class RL(object):
         pass
 
 
-# off-policy
-class QLearningTable(RL):
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-        super(QLearningTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+# backward eligibility traces
+class SarsaLambdaTable(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9, trace_decay=0.9):
+        super(SarsaLambdaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
 
-    def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
-        q_predict = self.q_table.ix[s, a]
-        if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.ix[s_, :].max()  # next state is not terminal
-        else:
-            q_target = r  # next state is terminal
-        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)  # update
+        # backward view, eligibility trace.
+        self.lambda_ = trace_decay
+        self.eligibility_trace = self.q_table.copy()
 
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            to_be_append = pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            self.q_table = self.q_table.append(to_be_append)
 
-# on-policy
-class SarsaTable(RL):
-
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-        super(SarsaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+            # also update eligibility trace
+            self.eligibility_trace = self.eligibility_trace.append(to_be_append)
 
     def learn(self, s, a, r, s_, a_):
-        self.check_state_exist(s_)# 检测下一个state
+        self.check_state_exist(s_)
         q_predict = self.q_table.ix[s, a]
         if s_ != 'terminal':
             q_target = r + self.gamma * self.q_table.ix[s_, a_]  # next state is not terminal
         else:
             q_target = r  # next state is terminal
-        self.q_table.ix[s, a] += self.lr * (q_target - q_predict) # update
+        error = q_target - q_predict
+
+        # increase trace amount for visited state-action pair
+
+        # Method 1:
+        # self.eligibility_trace.ix[s, a] += 1
+
+        # Method 2:
+        self.eligibility_trace.ix[s, :] *= 0
+        self.eligibility_trace.ix[s, a] = 1
+
+        # Q update
+        self.q_table += self.lr * error * self.eligibility_trace
+
+        # decay eligibility trace after update
+        self.eligibility_trace *= self.gamma*self.lambda_
